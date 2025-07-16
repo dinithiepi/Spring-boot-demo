c82fbc63e553144c25f6acc956c570223f93bcbb
Dinithi Epitakaduwa <dinithiepi@gmail.com>
2025-07-16 09:36:12 +0530

    spring boot

---
+++ src/main/java/com/example/demo/student/studentconfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class studentconfig {
+    @Bean
+    CommandLineRunner commandLineRunner(Studentrepository repository){
+        return args -> {
+            Student Thimal=new Student(
+                    "Thimal",
+                    "adeeshathimal@gmail.com",
+                    LocalDate.of(2002, Month.MARCH,31),
+                    23
+
+            );
+            Student Dinithi=new Student(
+                    "Dinithi",
+                    "dinithiepi@gmail.com",
+                    LocalDate.of(2002, Month.OCTOBER,17),
+                    23
+
+            );
+            repository.saveAll(List.of(Thimal,Dinithi));
+        };
+    }
+}
